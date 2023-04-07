cbdc860ff8f5021c160d189fa1791a3f675c3789
cpaca <37684627+cpaca@users.noreply.github.com>
2023-04-06 22:38:50 -0700

    Get rid of unnecessary datetime values
    No longer necessary now that I figured out how to get cProfile working.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from RegionTypes.RegionTypeMaker import create
 from testZ3 import run_tests
 from RegionHandler import RegionHandler
@@ -8,10 +6,6 @@ if __name__ == '__main__':
     # run_tests()
     regions = [create("1"), create("1"), create("2")]
     handler = RegionHandler(regions)
-    start = datetime.utcnow()
-    print("Start time:", start)
     # result = handler.test_cells([0, 11, 11, 11, 1, 11, 11, 11])
     result = handler.recursive_test(5)
-    end = datetime.utcnow()
-    print("End time:", end)
-    print("Time taken:", end - start)
+    print("Done")
